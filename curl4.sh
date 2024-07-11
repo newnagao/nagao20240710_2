e7cebd5369511e6136f9f164933d6b6cf2ca7d63
newnagao <nagao@hek.co.jp>
2024-07-11 13:53:41 +0900

    Update curl4.sh

--- curl4.sh
+++ curl4.sh
@@ -5,4 +5,4 @@ key_pm="api-key:ffec39680f7b4946a392784be5798e72"
 json="{\"model\": \"tts\", \"input\": \"$1\", \"voice\": \"alloy\"}"
 outputfilename="$2"
 
-curl -k "$url" -H "$key_pm" -H "Content-Type:application/json" -d "$json" --output "$outputfilename"
+curl -k https://onsei-kensho.openai.azure.com/openai/deployments/tts-model/audio/speech?api-version=2024-02-15-preview -H "api-key:ffec39680f7b4946a392784be5798e72" -H "Content-Type:application/json" -d "{\"model\": \"tts\", \"input\": \"$1\", \"voice\": \"alloy\"}" --output $2
